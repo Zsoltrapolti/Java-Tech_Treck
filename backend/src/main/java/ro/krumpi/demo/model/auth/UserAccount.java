5ea6db01c80efe3e9a23c3202c7a1c849091254b
Alina Macavei <macaveialina456@yahoo.com>
2025-12-09 17:34:55 +0200

    rename project + fix db
    git rebase --continue#git rebase --continue

---
+++ backend/src/main/java/ro/krumpi/demo/model/auth/UserAccount.java
@@ -0,0 +1,26 @@
+package ro.krumpi.demo.model.auth;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
---
+++ backend/src/main/java/ro/krumpi/demo/model/stock/Product.java
@@ -0,0 +1,21 @@
+package ro.krumpi.demo.model.stock;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    private String unitOfMeasure;
+    private Double quantity;
+
+    public Product(Long l, String product1, String type1, String unit1) {
+    }
+}
